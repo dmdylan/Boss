2a836a5cc58776e05f4168ee38f20d902073d7fc
dmdylan <dylandanowski@gmail.com>
2020-07-12 21:52:22 -0400

    Working on teleport ability

--- Assets/Scripts/Abilities/TeleportAbility.cs
+++ Assets/Scripts/Abilities/TeleportAbility.cs
@@ -8,11 +8,14 @@ public class TeleportAbility : BaseAbility
     [SerializeField] private float teleportDistance = 0;
     [SerializeField] private float teleportTravelTime = 0;
     [SerializeField] private float coolDown = 0;
+    [SerializeField] private GameObject teleportMarker = null;
+    private Camera abilityCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         Cooldown = coolDown;
+        abilityCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -29,9 +32,23 @@ public class TeleportAbility : BaseAbility
     public override IEnumerator UseAbility()
     {
         isOffCooldown = false;
-        StartCoroutine(StartCooldown());
 
-        transform.DOMove(transform.position + transform.forward * teleportDistance, teleportTravelTime).SetEase(Ease.Flash);
+        Ray ray = abilityCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 15f))
+        {
+            GameObject teleporterMarker = Instantiate(teleportMarker);
+            //this code crashes unity
+            while (hit.collider)
+            {
+                teleporterMarker.transform.position = hit.point;      
+            }
+        }
+
+
+        //transform.DOMove(transform.position + transform.forward * teleportDistance, teleportTravelTime).SetEase(Ease.Flash);
         yield return new WaitForEndOfFrame();
+        StartCoroutine(StartCooldown());
     }
 }
