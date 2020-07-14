8e446f1ef96ad83b6adcd6eccddc9015fdc79994
dmdylan <dylandanowski@gmail.com>
2020-07-14 16:52:57 -0400

    Cleaned up teleport. Adding character and attacks.

--- Assets/Scripts/Abilities/TeleportAbility.cs
+++ Assets/Scripts/Abilities/TeleportAbility.cs
@@ -8,7 +8,6 @@ using UnityEngine.WSA;
 public class TeleportAbility : BaseAbility
 {
     [SerializeField] private float teleportDistance = 0;
-    [SerializeField] private float teleportTravelTime = 0;
     [SerializeField] private float coolDown = 0;
     [SerializeField] private GameObject teleportMarker = null;
     private Camera abilityCamera;
@@ -29,7 +28,7 @@ public class TeleportAbility : BaseAbility
 
         if(!isOffCooldown) { return; }
 
-        StartCoroutine(UseAbility()); //Need vector3 position from camera;
+        StartCoroutine(UseAbility());
     }
 
     public override IEnumerator UseAbility()
@@ -41,29 +40,24 @@ public class TeleportAbility : BaseAbility
         while (isSelectingLocation)
         {
             Ray ray = abilityCamera.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(abilityCamera.transform.position, abilityCamera.transform.forward*15f, Color.red);
-            Debug.Log("In the loop");
 
-            if (Physics.Raycast(ray, out RaycastHit hit, 15f, layer))
+            if (Physics.Raycast(ray, out RaycastHit hit, teleportDistance, layer))
             {
-                Debug.Log("RayCast Hit Somthing");
                 teleportMarker.SetActive(true);
                 teleportMarker.transform.position = hit.point;         
             }
             else
             {
-                Debug.Log("Raycast hitting nothing");
                 teleportMarker.SetActive(false);
             }
 
             if (Input.GetKeyDown(KeyCode.Mouse1))
             {
-                Debug.Log("Right mouse clicked");
                 isSelectingLocation = false;
                 teleportMarker.SetActive(false);
             }
 
-            if (Input.GetKeyDown(KeyCode.Mouse0))
+            if (Input.GetKeyDown(KeyCode.Mouse0) && Physics.Raycast(ray, teleportDistance, layer))
             {
                 isSelectingLocation = false;
                 transform.DOMove(teleportMarker.transform.position, 0).SetEase(Ease.Flash);
@@ -73,7 +67,6 @@ public class TeleportAbility : BaseAbility
             yield return null;
         }
         
-        //transform.DOMove(transform.position + transform.forward * teleportDistance, teleportTravelTime).SetEase(Ease.Flash);
         yield return new WaitForEndOfFrame();
         StartCoroutine(StartCooldown());
     }
